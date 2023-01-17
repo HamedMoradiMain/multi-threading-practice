7976553004489e00ac88e5207f439c2b8a3a8087
Hamed Moradi <hamdmrady63@gmail.com>
2023-01-17 14:42:05 +0430

    practice

--- video4.py
+++ video4.py
@@ -1,4 +1,4 @@
-import multiprocessing 
+'''import multiprocessing 
 def calc_square(numbers, result):
 	for idx, n in enumerate(numbers):
 			result[idx] = n*n
@@ -8,4 +8,17 @@ if __name__ == "__main__":
 		p = multiprocessing.Process(target=calc_square,args=(numbers,result))
 		p.start()
 		p.join()
-		print('outside process',str(result[:]))
+		print('outside process',str(result[:]))'''
+import multiprocessing 
+def calc_square(numbers, result,v):
+		v.value = 5.6
+		for idx, n in enumerate(numbers):
+			result[idx] = n*n
+if __name__ == "__main__":
+		numbers = [2,3,5]
+		result = multiprocessing.Array('i',3)
+		v = multiprocessing.Value('d',0.0)
+		p = multiprocessing.Process(target=calc_square,args=(numbers,result,v))
+		p.start()
+		p.join()
+		print(v.value)
